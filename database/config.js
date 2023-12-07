9987c90c317a167adebe37a9c36ddfb5215aa44f
Vivyan06 <114101590+Vivyan06@users.noreply.github.com>
2023-12-07 11:27:53 -0600

    segunda subida

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://daniela:danielaruiz@cluster1.8zhrttp.mongodb.net/salonDB?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://vivyanjoselin:6Demarzo@cluster0.ytjesa7.mongodb.net/salondb?retryWrites=true&w=majority')
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
